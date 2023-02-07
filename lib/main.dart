8e0876a9a9fe564e049ffbe11e7c6031329e5dfc
Fausto Andino <fausto1andino@gmail.com>
2023-02-06 20:58:59 -0500

    Orientacion del login Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:gastos_foraneo/src/pages/login.page.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -10,12 +10,11 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
-  runApp(const MyApp());
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((value) => runApp(const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-
   const MyApp({super.key});
   // This widget is the root of your application.
   @override
